91fe70fd8f1a596bbd1fead1435a47bd520739cd
pankajxyz <pankajxyz@gmail.com>
2015-11-22 10:59:02 +0800

    Fixed the merged error. Added two other files showing other ways to merge the tables.

--- que.py
+++ que.py
@@ -9,6 +9,7 @@ dfnew = dfnew.drop_duplicates().fillna(0)
 dfold=dfold.drop_duplicates().fillna(0)
 print dfnew.tail(5)
 print dfold.tail(5)
-dfupdate=pd.merge(dfold, dfnew, on='Date')
+#dfupdate=pd.merge(dfold, dfnew, on='Date') # Pankaj: This won't work because you have declared Date column as index while reading file. 
+dfupdate=pd.merge(dfold, dfnew, left_index=True, right_index=True) # Here I am telling to join on index column from both tables.
 dfupdate.to_csv('NiftyFirst.csv', index=True)
 
